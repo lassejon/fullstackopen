8e347a8cbe2708c017684b427cea6a95f3a29a39
lassejon <lasss@live.dk>
2022-02-08 14:08:49 +0100

    found uncommited stuff

--- part1/src/App.js
+++ part1/src/App.js
@@ -3,22 +3,23 @@ import React from 'react';
 const Hello = (props) => {
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old.</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
   )
 }
 
 const App = () => {
-  const name = 'Peter';
-  const age = 10;
+  const name = 'Peter'
+  const age = 10
 
   return (
-    <>
+    <div>
       <h1>Greetings</h1>
-      <Hello name={name} age={age}/>
-      <Hello name="Leif" age={26 + age} />
-    
-    </>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
   )
 }
 
