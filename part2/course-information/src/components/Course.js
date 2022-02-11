3f58711306106ec4ad129eca3a5d4eb73768eb15
lassejon <lasss@live.dk>
2022-02-11 08:56:37 +0100

    cleaned up

--- part2/course-information/src/components/Course.js
+++ part2/course-information/src/components/Course.js
@@ -1,14 +1,13 @@
 import React from "react"
 
-const Header = (props) => {
-    const name = props.props.course.name
+const Header = ({course}) => {
+    const name = course.name
     return (
       <h1>{name}</h1>
     )
   }
   
-  const Part = (props) => {
-    const part = props.part;
+  const Part = ({part}) => {
     return (
       <div>
         {part.name} {part.exercises}
@@ -16,8 +15,8 @@ const Header = (props) => {
     )
   }
   
-  const Content = (props) => {
-    const parts = props.props.course.parts
+  const Content = ({course}) => {
+    const parts = course.parts
     return (
       <div>
         {parts.map(part => <Part key={part.id} part={part} />)}
@@ -25,19 +24,19 @@ const Header = (props) => {
     )
   }
   
-  const Total = (props) => {
-    const parts = props.props.course.parts;
+  const Total = ({course}) => {
+    const parts = course.parts;
     return (
       <p><b>total of exercises {parts.reduce((result, current) => result + current.exercises, 0)}</b></p>
     )
   }
   
-  const Course = (course) => {
+  const Course = ({course}) => {
     return (
       <>
-        <Header props={course}/>
-        <Content props={course}/>
-        <Total props={course}/>
+        <Header course={course}/>
+        <Content course={course}/>
+        <Total course={course}/>
       </>
     )
   }
